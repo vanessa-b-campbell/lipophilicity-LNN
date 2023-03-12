42529fe57565293e6127f1c6ab47c1c410b86e36
Vanessa Campbell <122318527+vanessa-b-campbell@users.noreply.github.com>
2023-03-11 20:55:32 -0500

    Add files via upload

---
+++ lipoNN.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+import matplotlib.pyplot as plt
+
+class LipoNet():
+    def __init__(self):
+        pass
+
+    
