06cb6ffec7bdcd7ccd973bb7f3aa83f6ddc94225
Vanessa Campbell <122318527+vanessa-b-campbell@users.noreply.github.com>
2023-06-27 15:31:25 -0400

    Add files via upload

--- Lipo_train_model.py
+++ Lipo_train_model.py
@@ -35,31 +35,35 @@ train_set, val_set = torch.utils.data.random_split(
 print(train_set)
 
 
-#%%
-# Build pytorch training and validation set dataloaders:
-train_dataloader = DataLoader(train_set, batch_size = 32, shuffle=True)
-val_dataloader = DataLoader(val_set, batch_size = 32, shuffle=True)
-
-
-
-
-
-#%%
-## RUN TRAINING LOOP: ---
 
 # Train with a random seed to initialize weights:
 torch.manual_seed(0)
 
 # Assign training to a device (often cpu when we are just starting out)
 device = torch.device("cpu")
-
 #################################################################################### hyperparameters
-model = LipoNet(256)
+
+
+model = LipoNet(800, 500, 256)
 model.to(device)
-epoch = 30
+epoch = 100
+learn_rate = 0.001
+batch_size = 32
+
+
+
+
+#%%
+# Build pytorch training and validation set dataloaders:
+train_dataloader = DataLoader(train_set, batch_size, shuffle=True)
+val_dataloader = DataLoader(val_set, batch_size, shuffle=True)
+
+
+#%%
+## RUN TRAINING LOOP: ---
 
 # Set up optimizer:
-optimizer = optim.Adam(model.parameters(), lr=0.001) # learning rate ex: 1*10^-3
+optimizer = optim.Adam(model.parameters(), lr = learn_rate) # learning rate ex: 1*10^-3
 
 train_losses = []
 val_losses = []
@@ -81,6 +85,23 @@ print("Time Elapsed = {}s".format(end_time - start_time))
 # %%
 
 
+#%% Model Statistics
+
+input_all, target_all, pred_prob_all = predict(model, device, val_dataloader)
+
+# print("input_all: {i}".format(i = input_all))
+# print("target_all: {t}".format(t = target_all))
+# print("pred_prob_all: {p}".format(p = pred_prob_all))
+
+r2_function = r2_score(target_all, pred_prob_all)
+mae = mean_absolute_error(target_all, pred_prob_all)
+rmse = mean_squared_error(target_all, pred_prob_all, squared=False)
+
+# only a few digits are relevant
+print("R2 Score: {:.4f}".format(r2_function))
+print("MAE: {:.4f}".format(mae))
+print("RMSE: {:.4f}".format(rmse))
+
 # plotting loss vs epochs for validation and training
 #fig1 = plt.figure()
 plt.plot(train_losses, label ='train losses')
@@ -95,21 +116,6 @@ plt.xlabel('time')
 plt.ylabel('losses')
 
 plt.show()
-
-
-#%% Model Statistics
-
-input_all, target_all, pred_prob_all = predict(model, device, val_dataloader)
-
-r2_function = r2_score(target_all, pred_prob_all)
-mae = mean_absolute_error(target_all, pred_prob_all)
-rmse = mean_squared_error(target_all, pred_prob_all, squared=False)
-
-# only a few digits are relevant
-print("R2 Score: {:.4f}".format(r2_function))
-print("MAE: {:.4f}".format(mae))
-print("RMSE: {:.4f}".format(rmse))
-
 
 
 plt.figure(figsize=(4, 4), dpi=100)
