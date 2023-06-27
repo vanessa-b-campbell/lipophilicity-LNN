207b180107c96f8ca2363f0b16547a4d0c1bf4a8
Vanessa Campbell <122318527+vanessa-b-campbell@users.noreply.github.com>
2023-06-27 15:32:18 -0400

    Add files via upload

--- LipoNet.py
+++ LipoNet.py
@@ -8,27 +8,36 @@ import torch.nn.functional as F
 # training set input vector is 1024 columns
 # training set outout targets is a 1 column
 class LipoNet(nn.Module):
-    def __init__(self, hidden_layer_size = 256):
+    def __init__(self, hidden_layer_size_1 = 256, hidden_layer_size_2 = 256, hidden_layer_size_3 = 256):
         super(LipoNet, self).__init__()
         # super is saying that class LipoNet is inheriting traits from nn.Module class
         input_size = 1024 
         output_size = 1
-
+# list of layers - in foreard for len(list) add relu(x)-- ask emily 
         # two linear layers
-        self.fc1 = nn.Linear(input_size, hidden_layer_size) #(raw training dataset)
+        self.fc1 = nn.Linear(input_size, hidden_layer_size_1) #(raw training dataset)
         # nn.Linear(size of dataset, size of output)
-        self.fc2_5 = nn.Linear(hidden_layer_size, hidden_layer_size)
-        self.fc2 = nn.Linear(hidden_layer_size, output_size)
-        self.dp1 = nn.Dropout(0.5)
-        self.dp2 = nn.Dropout(0.5)
+        self.fc2_a = nn.Linear(hidden_layer_size_1, hidden_layer_size_2)
+        self.fc2_b = nn.Linear(hidden_layer_size_2, hidden_layer_size_3)
+    
+        self.fc2 = nn.Linear(hidden_layer_size_3, output_size)
+        #self.dp1 = nn.Dropout(0.4)
+        #self.dp2 = nn.Dropout(0.5)
     
     def forward(self, x):
         x = self.fc1(x)
-        x =self.dp1(x)
+        #x =self.dp1(x)
         x = F.relu(x)
-        x = self.fc2_5(x)
-        x =self.dp1(x)
+
+        x = self.fc2_a(x)
+        #x =self.dp1(x)
         x = F.relu(x)
+
+        x = self.fc2_b(x)
+        x = F.relu(x)
+
+    
+
         x = self.fc2(x)
         # output (op) will have the same dimensions as the target output (1)
         return x #op
